94656a9c9fd6986496a9fc06f7dc3463a27ba4f3
or-tarazi <tarazi.or@gmail.com>
2025-05-01 23:46:16 +0300

    update from aquarium

--- MyThread.cpp
+++ MyThread.cpp
@@ -2,11 +2,23 @@
 // Created by or.tarazi1 on 4/24/25.
 //
 
+#include <cstdlib>
+#include <iostream>
 #include "MyThread.h"
 
 MyThread::MyThread(int tid, thread_entry_point entry_point){
     this->tid = tid;
     this->entry_point = entry_point;
+    this->state = READY;
+    this->quantums_left = 0;
+    if(tid != 0) {
+        try {
+            this->stack = new char[STACK_SIZE];
+        } catch (const std::bad_alloc &e) {
+            std::cerr << "system error: memory allocation failed\n";
+            exit(1);
+        }
+    }
 }
 
 int MyThread::get_tid() {
@@ -14,6 +26,37 @@ int MyThread::get_tid() {
 }
 
 void MyThread::set_tid(int new_tid) {
-    this->tid = tid;
+    this->tid = new_tid;
 }
+
+ThreadState MyThread::get_state() {
+    return this->state;
+}
+
+void MyThread::set_state(ThreadState new_state) {
+    this->state = new_state;
+}
+
+void MyThread::set_quantums_left(int quantums) {
+    this->quantums_left = quantums;
+}
+
+int MyThread::get_quantums_left() {
+    return this->quantums_left;
+}
+
+void MyThread::set_running_quantums(int added_quantums) {
+    this->running_quantums += added_quantums;
+
+}
+
+int MyThread::get_running_quantums() {
+    return this->running_quantums;
+}
+
+thread_entry_point MyThread::get_entry_point() {
+    return this->entry_point;
+}
+
+
 
--- MyThread.h
+++ MyThread.h
@@ -4,18 +4,38 @@
 
 #ifndef OS_EX2_MYTHREAD_H
 #define OS_EX2_MYTHREAD_H
+#include "uthreads.h"
+#include <setjmp.h>
+
+enum ThreadState {
+    READY,
+    BLOCKED,
+    RUNNING
+};
 
 typedef void (*thread_entry_point)(void);
 
 class MyThread{
-
-    MyThread(int tid, thread_entry_point entry_point);
-    int get_tid();
-    void set_tid(int tid);
+    public:
+        MyThread(int tid, thread_entry_point entry_point);
+        int get_tid();
+        void set_tid(int tid);
+        int get_quantums_left();
+        void set_quantums_left(int quantums);
+        int get_running_quantums();
+        void set_running_quantums(int added_quantums);
+        ThreadState get_state();
+        void set_state(ThreadState);
+        char* stack;
+        sigjmp_buf env;
+        thread_entry_point get_entry_point();
 
     private:
         thread_entry_point entry_point;
         int tid;
+        ThreadState state;
+        int quantums_left;
+        int running_quantums;
 };
 
 
